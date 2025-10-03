ac427ba94d37597e1e3bd1e727d93d31ed5d0316
Whunk <hannes.a.andersson@gmail.com>
2025-10-03 20:01:18 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  }
+    base: '/never-break-a-stride/', // <-- this is critical
 });
