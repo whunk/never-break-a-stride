1ddeec0ae788a2d8c7de8b62bfcd5ae5e96d293e
Whunk <hannes.a.andersson@gmail.com>
2025-10-30 12:50:54 +0100

    remove tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/postcss";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),],
     base: '/never-break-a-stride/',
     build:{
       target: 'es2015',
